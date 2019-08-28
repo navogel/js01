170bdaad04a0d4c44484e69f5bc916d4e77c2527
Nate V <navogel@gmail.com>
2019-08-28 09:09:07 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("hello main.js, great jorb");
 
 let name = "NATER";
 
-console.log(name)
+console.log(name);
 
 //let matches = document.querySelectorAll(".article__header");
 
@@ -11,7 +11,6 @@ console.log(name)
 //  matches.forEach(function(i) {
 //      console.log(i);
 //  });
-
 
 //change title to name
 
@@ -22,14 +21,20 @@ header.textContent = `Welcome to the ${name} blog`;
 //select all .article__header elements
 
 let allHeaders = document.querySelectorAll(".article__header");
-console.log(allHeaders);
+console.log("all headers", allHeaders);
 
-//change each one to add class "important"
+//change each one to add class "important" using traditional for each
 
-for (var i = 0; i < allHeaders.length; i++) {
-    allHeaders[i].classList.add("important");
-    console.log(allHeaders[i].className);
+for (let i = 0; i < allHeaders.length; i++) {
+	allHeaders[i].classList.add("important");
+	console.log(allHeaders[i].className);
 }
+
+//using shorthand for each
+
+allHeaders.forEach(function(unit) {
+	unit.classList.add("important");
+});
 
 //remove .dashed class
 
@@ -40,6 +45,3 @@ dashed.classList.remove("dashed");
 
 let footerEl = document.querySelector(".article__footer");
 footerEl.classList.add("goldenrod");
-
-
-
